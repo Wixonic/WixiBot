69aade8da06e2b38571ed48aae146de7ee07c796
Wixonic <contact@wixonic.fr>
2024-12-05 19:15:35 +0100

    Update embeds descriptions

--- commands/radio.js
+++ commands/radio.js
@@ -200,21 +200,21 @@ module.exports = {
 									label: "Watch on YouTube",
 									style: ButtonStyle.Link,
 									type: ComponentType.Button,
-									url: `https://www.youtube.com/watch?v=${song.youtubeId}`
+									url: `https://www.youtube.com/watch?v=${Radio.song.youtubeId}`
 								}]
 							}],
 							content: `Song resumed: playing ${Radio.song.track} by ${Radio.song.artist}.`,
 							embeds: [{
-								title: song.track,
-								description: `This song has been added to the waiting list.\n${Radio.waitingList.length > 1 ? (Radio.waitingList.length == 2 ? "One song remaining." : `${Radio.waitingList.length} songs remaining.`) : "The next song will be this one."}`,
+								title: Radio.song.track,
+								description: `This song is currently playing at <#${Radio.channel.id}>.`,
 								author: {
-									name: song.artist,
+									name: Radio.song.artist,
 									icon_url: `https://cdn.discordapp.com/app-assets/${config.discord.application.clientId}/${config.discord.application.assets.youtube}.png`
 								},
 								thumbnail: {
-									url: song.spotifyArtworkURL
+									url: Radio.song.spotifyArtworkURL
 								},
-								color: hexToIntColor(song.color)
+								color: hexToIntColor(Radio.song.color)
 							}],
 							ephemeral: true
 						});
@@ -250,7 +250,7 @@ module.exports = {
 						content: `Skipping ${previousSongName}, and playing ${Radio.song.track} by ${Radio.song.artist}.`,
 						embeds: [{
 							title: Radio.song.track,
-							description: `This song has been added to the waiting list.\n${Radio.waitingList.length > 1 ? (Radio.waitingList.length == 2 ? "One song remaining." : `${Radio.waitingList.length} songs remaining.`) : "The next song will be this one."}`,
+							description: `This song is currently playing at <#${Radio.channel.id}>.`,
 							author: {
 								name: Radio.song.artist,
 								icon_url: `https://cdn.discordapp.com/app-assets/${config.discord.application.clientId}/${config.discord.application.assets.youtube}.png`
