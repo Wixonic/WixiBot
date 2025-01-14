9b62207e8cc98a318eb4386e88a1c008d99c37fa
Wixonic <contact@wixonic.fr>
2025-01-14 21:30:00 +0100

    Add Blender RPC

--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
 const { RichPresence } = require("discord.js-selfbot-v13");
+const http = require("http");
 
 const log = require("../log.js");
 
@@ -13,6 +14,133 @@ const wt = require("./warthunder.js");
 
 const config = require("../config.js");
 
+let blenderData = {
+	date: 0
+};
+
+const server = http.createServer((req, res) => {
+	let body = "";
+
+	req.on("data", (chunk) => {
+		body += chunk.toString();
+	});
+
+	req.on("end", () => {
+		try {
+			blenderData = JSON.parse(body);
+			blenderData.date = Date.now();
+
+			res.writeHead(200).end("OK");
+		} catch (e) {
+			blenderData = null;
+			res.writeHead(400).end("Bad content");
+		}
+	});
+});
+
+server.listen(config.port.blenderServer, () => log(`Blender server listening on :${config.port.blenderServer}`));
+
+const processBlender = async () => {
+	if (!blenderData || blenderData.date + 30 * 1000 < Date.now()) {
+		blenderData = null;
+		discord.removeActivity("blender");
+	} else {
+		discord.addActivity("blender", {
+			applicationId: config.discord.application.clientId,
+			assets: {
+				small_image: `https://cdn.discordapp.com/app-assets/${config.discord.application.clientId}/${config.discord.application.assets[blenderData.small_image]}.png`,
+				small_text: blenderData.small_text,
+				large_image: `https://cdn.discordapp.com/app-assets/${config.discord.application.clientId}/${config.discord.application.assets[blenderData.large_image]}.png`,
+				large_text: blenderData.large_text
+			},
+			timestamps: {
+				start: blenderData.startDate
+			},
+			name: "Blender",
+			details: blenderData.details,
+			state: blenderData.state,
+			type: 0 // PLAYING
+		});
+	}
+};
+
+
+let lastPronoteUpdate = 0;
+
+const processPronote = async () => {
+	if (lastPronoteUpdate + 60 * 1000 < Date.now()) {
+		const currentClass = await pronote.requestClassAt(new Date());
+
+		if (!currentClass) discord.removeActivity("pronote");
+		else {
+			discord.addActivity("pronote", {
+				applicationId: config.discord.application.clientId,
+				assets: {
+					large_image: `https://cdn.discordapp.com/app-assets/${config.discord.application.clientId}/${config.discord.application.assets.clock}.png`,
+					large_text: `Ends at ${String(currentClass.endDate.getHours()).padStart(2, "0")}:${String(currentClass.endDate.getMinutes()).padStart(2, "0")} UTC+${-currentClass.endDate.getTimezoneOffset() / 60}`
+				},
+				name: currentClass.subject,
+				details: currentClass.subject,
+				state: `Started at ${String(currentClass.startDate.getHours()).padStart(2, "0")}:${String(currentClass.startDate.getMinutes()).padStart(2, "0")} UTC+${-currentClass.startDate.getTimezoneOffset() / 60}`,
+				type: 5 // COMPETING
+			});
+		}
+
+		lastPronoteUpdate = Date.now();
+	}
+};
+
+
+let lastMapRefresh = 0;
+let inWarThunderGameSince = null;
+
+const processWarThunder = async () => {
+	const data = await wt();
+
+	if (data.valid) {
+		if (!inWarThunderGameSince) inWarThunderGameSince = Date.now();
+		if (lastMapRefresh + 30 * 1000 < Date.now()) {
+			const getImage = async () => {
+				await request({
+					url: "https://server.wixonic.fr/warthundermap.png",
+					method: "POST",
+					headers: {
+						authorization: `WixKey ${config.wixkey}`,
+						"content-type": "image/png"
+					},
+					secure: false,
+					type: "raw",
+					body: data.map.toString("base64url")
+				});
+
+				return await RichPresence.getExternal(discord.client, config.discord.application.clientId, `https://server.wixonic.fr/warthundermap.png?t=${Date.now()}`);
+			};
+
+			const mapImage = await getImage();
+
+			discord.addActivity("wt", {
+				applicationId: config.discord.application.clientId,
+				assets: {
+					large_image: mapImage[0].external_asset_path,
+					large_text: data.vehicle,
+					small_image: `https://cdn.discordapp.com/app-assets/${config.discord.application.clientId}/${config.discord.application.assets.war_thunder}.png`,
+					small_text: "War Thunder"
+				},
+				timestamps: {
+					start: inWarThunderGameSince
+				},
+				name: "War Thunder",
+				details: data.vehicle,
+				type: 0 // PLAYING
+			});
+
+			lastMapRefresh = Date.now();
+		}
+	} else {
+		discord.removeActivity("wt");
+		inWarThunderGameSince = null;
+	}
+};
 
 /**
  * @type {import("../types.js").Song?}
@@ -78,90 +206,12 @@ const processTrack = async (song) => {
 };
 
 
-let lastMapRefresh = 0;
-let inWarThunderGameSince = null;
-
-const processWarThunder = async () => {
-	const data = await wt();
-
-	if (data.valid) {
-		if (!inWarThunderGameSince) inWarThunderGameSince = Date.now();
-		if (lastMapRefresh + 30 * 1000 < Date.now()) {
-			const getImage = async () => {
-				await request({
-					url: "https://server.wixonic.fr/warthundermap.png",
-					method: "POST",
-					headers: {
-						authorization: `WixKey ${config.wixkey}`,
-						"content-type": "image/png"
-					},
-					secure: false,
-					type: "raw",
-					body: data.map.toString("base64url")
-				});
-
-				return await RichPresence.getExternal(discord.client, config.discord.application.clientId, `https://server.wixonic.fr/warthundermap.png?t=${Date.now()}`);
-			};
-
-			const mapImage = await getImage();
-
-			discord.addActivity("wt", {
-				applicationId: config.discord.application.clientId,
-				assets: {
-					large_image: mapImage[0].external_asset_path,
-					large_text: data.vehicle,
-					small_image: `https://cdn.discordapp.com/app-assets/${config.discord.application.clientId}/${config.discord.application.assets.war_thunder}.png`,
-					small_text: "War Thunder"
-				},
-				timestamps: {
-					start: inWarThunderGameSince
-				},
-				name: "War Thunder",
-				details: data.vehicle,
-				type: 0 // PLAYING
-			});
-
-			lastMapRefresh = Date.now();
-		}
-	} else {
-		discord.removeActivity("wt");
-		inWarThunderGameSince = null;
-	}
-};
-
-
-let lastPronoteUpdate = 0;
-
-const processPronote = async () => {
-	if (lastPronoteUpdate + 60 * 1000 < Date.now()) {
-		const currentClass = await pronote.requestClassAt(new Date());
-
-		if (!currentClass) discord.removeActivity("pronote");
-		else {
-			discord.addActivity("pronote", {
-				applicationId: config.discord.application.clientId,
-				assets: {
-					large_image: `https://cdn.discordapp.com/app-assets/${config.discord.application.clientId}/${config.discord.application.assets.clock}.png`,
-					large_text: `Ends at ${String(currentClass.endDate.getHours()).padStart(2, "0")}:${String(currentClass.endDate.getMinutes()).padStart(2, "0")} UTC+${-currentClass.endDate.getTimezoneOffset() / 60}`
-				},
-				name: currentClass.subject,
-				details: currentClass.subject,
-				state: `Started at ${String(currentClass.startDate.getHours()).padStart(2, "0")}:${String(currentClass.startDate.getMinutes()).padStart(2, "0")} UTC+${-currentClass.startDate.getTimezoneOffset() / 60}`,
-				type: 5 // COMPETING
-			});
-		}
-
-		lastPronoteUpdate = Date.now();
-	}
-};
-
-
 const main = async () => {
 	await discord.ready();
-
+	await processBlender();
+	await processPronote();
 	await processTrack(await getCurrentTrackInfo());
 	await processWarThunder();
-	await processPronote();
 
 	setTimeout(main, 2500);
 };
