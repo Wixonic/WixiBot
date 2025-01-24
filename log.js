b5784479112a35316abbcdd96853fe7f44e70e0d
Wixonic <contact@wixonic.fr>
2025-01-24 19:17:58 +0100

    Fix log

--- log.js
+++ log.js
@@ -6,11 +6,12 @@ const log = (any) => {
 	console.log(`[${now.toLocaleDateString("fr", { day: "2-digit", month: "2-digit", year: "numeric" })} ${now.toLocaleTimeString("en", { hour12: false, hour: "2-digit", minute: "2-digit", second: "2-digit", fractionalSecondDigits: 3 })}] ${typeof any == "string" ? any : JSON.stringify(any, null, 2)}`);
 };
 
-log.error = (any) => {
+log.error = async (any) => {
 	log(any);
 
 	if (settings.log.active) {
-		client.guilds.cache.get(settings.log.guild).channels.cache.get(settings.log.channel).send({
+		const guild = (client.guilds.cache.get(settings.log.guild) ?? await client.guilds.fetch(settings.log.guild));
+		await (guild.channels.cache.get(settings.log.channel) ?? await guild.channels.fetch(settings.log.channel)).send({
 			content: `\`\`\`\n[ERROR]: ${typeof any == "string" ? any : JSON.stringify(any, null, 2)}\`\`\``
 		});
 	}
