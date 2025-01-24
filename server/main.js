948a6d2bfeea0aebfeba17a7c316dbc455fe9628
Wixonic <contact@wixonic.fr>
2025-01-24 19:12:51 +0100

    Update

--- server/main.js
+++ server/main.js
@@ -85,7 +85,7 @@ const processWarThunder = async () => {
 		if (lastMapRefresh + 30 * 1000 < Date.now()) {
 			const getImage = async () => {
 				await request({
-					url: "https://server.wixonic.fr/warthundermap.png",
+					url: "https://server.wixonic.fr/warthunder/warthundermap.png",
 					method: "POST",
 					headers: {
 						authorization: `WixKey ${config.wixkey}`,
@@ -96,7 +96,7 @@ const processWarThunder = async () => {
 					body: data.map.toString("base64url")
 				});
 
-				return await RichPresence.getExternal(discord.client, config.discord.application.clientId, `https://server.wixonic.fr/warthundermap.png?t=${Date.now()}`);
+				return await RichPresence.getExternal(discord.client, config.discord.application.clientId, `https://server.wixonic.fr/warthunder/warthundermap.png?t=${Date.now()}`);
 			};
 
 			const mapImage = await getImage();
