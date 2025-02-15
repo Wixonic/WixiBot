01f8d2b2593f8b576225cefa021507212e1a6490
Wixonic <contact@wixonic.fr>
2025-02-15 21:18:49 +0100

    Add message as command

---
+++ src/commands/message-as.js
@@ -0,0 +1,48 @@
+const { ApplicationCommandType, ApplicationCommandOptionType, ChannelType, MessageFlags, PermissionFlagsBits } = require("discord.js");
+
+/**
+ * @type {CommandInfo}
+ */
+const info = {
+	name: "Message As",
+	deploy: {
+		type: ApplicationCommandType.ChatInput,
+		name: "message-as",
+		description: "Sends a message as the bot in the specified channel",
+		default_member_permissions: PermissionFlagsBits.ManageMessages.toString(),
+		options: [
+			{
+				type: ApplicationCommandOptionType.String,
+				name: "content",
+				description: "The content of the message to be sent",
+				required: true
+			}, {
+				type: ApplicationCommandOptionType.Channel,
+				name: "channel",
+				description: "The channel where the message will be sent",
+				required: true,
+				channel_types: [
+					ChannelType.GuildAnnouncement,
+					ChannelType.GuildText
+				]
+			}
+		]
+	},
+
+	/**
+	 * @param {import("discord.js").CommandInteraction} interaction
+	 */
+	run: async (bot, logger, interaction) => {
+		await interaction.deferReply({
+			flags: MessageFlags.Ephemeral
+		});
+
+		const content = interaction.options.getString("content");
+		const channel = interaction.options.getChannel("channel");
+
+		await channel.send(content);
+		await interaction.deleteReply();
+	}
+};
+
+module.exports = info;
