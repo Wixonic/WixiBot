4210f5c92694eef6e2fdbe5c419deab0bdfa192b
Wixonic <contact@wixonic.fr>
2025-02-19 17:46:38 +0100

    Types update

---
+++ src/types.d.ts
@@ -0,0 +1,88 @@
+export interface ApplicationSettings {
+	clientId: string;
+	clientSecret: SecretsDiscordClientSettings["secret"];
+	publicKey: string;
+	token: SecretsDiscordClientSettings["token"];
+	commands: CommandsSettings;
+};
+
+export interface CommandsSettings {
+	ticket: {
+		channel: string;
+	};
+	rules: {
+		channel: string;
+	};
+};
+
+export interface PathsSettings {
+
+};
+
+export interface SecretsDiscordApplicationSettings {
+	secret: string;
+	token: string;
+};
+
+export interface SecretsDiscordClientSettings {
+	token: string;
+};
+
+export interface SecretsDiscordSettings {
+	application: SecretsDiscordApplicationSettings;
+	client: SecretsDiscordClientSettings;
+	webhook: string;
+}
+
+export interface ServerSecretsSettings {
+	cert: string;
+	key: string;
+};
+
+export interface SecretsSettings {
+	discord: SecretsDiscordSettings;
+	server: ServerSecretsSettings;
+};
+
+export interface MainSettings {
+	active: boolean;
+	port: number;
+	application: ApplicationSettings;
+	paths: PathsSettings;
+	secrets: SecretsSettings;
+};
+
+
+export type RequestMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE" | "OPTIONS";
+export type RequestResponseType = "headers" | "json" | "raw" | "text";
+
+export interface RequestOptions {
+	auth?: string;
+	body?: object;
+	headers?: import("http").OutgoingHttpHeaders;
+	method?: RequestMethod;
+	secure?: boolean;
+	type: RequestResponseType;
+	url: URL | string;
+};
+
+
+export interface CommandOptions {
+	id: string;
+	name: string;
+	path: string;
+};
+
+
+export interface CommandInfo {
+	deploy: import("discord.js").APIApplicationCommand;
+	name: string;
+	run: (bot: import("./bot.js"), logger: Logger, ...any: any[]) => Promise<void>;
+};
+
+
+export interface ListenerInfo {
+	id: string;
+	name: string;
+	run: (bot: import("./bot.js"), logger: Logger, ...any: any[]) => Promise<void>;
+};
