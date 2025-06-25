6540984253fd8aa9f53533fd549686317d427584
Wixonic <contact@wixonic.fr>
2025-06-25 20:16:56 +0200

    Update activity roles values

--- src/settings/1179518852846067833/roles.js
+++ src/settings/1179518852846067833/roles.js
@@ -63,13 +63,13 @@ module.exports = {
 		{
 			name: "Easter",
 			color: "#C5EBD5",
-			from: "03-23T00:00:00",
-			to: "04-25T23:59:59"
+			from: "04-01T00:00:00",
+			to: "04-07T23:59:59"
 		},
 		{
 			name: "Summer",
 			color: "#47927E",
-			from: "06-01T00:00:00",
+			from: "08-01T00:00:00",
 			to: "08-31T23:59:59"
 		},
 		{
