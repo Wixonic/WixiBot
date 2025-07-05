077f1d42e2e570f9df6a5c0f78f0f55067bdd3b4
Wixonic <contact@wixonic.fr>
2025-07-05 17:07:25 +0200

    Subcommands and subcommand groups support

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,5 +1,7 @@
-const { ApplicationCommandType, MessageFlags } = require("discord.js");
+const { ApplicationCommandType, ApplicationCommandOptionType, MessageFlags } = require("discord.js");
 const fs = require("fs");
+
+const { sendLongMessage } = require("../lib/utils.js");
 
 /**
  * @type {import("../types.d.ts").CommandInfo}
@@ -27,12 +29,40 @@ const info = {
 			const guildCommands = guild?.commands ? await guild.commands.fetch() : new Collection();
 			const globalCommands = await bot.application.commands.fetch();
 
+			/**
+			 * @param {import("discord.js").ApplicationCommand} command
+			 * @returns {string}
+			 */
+			const displayCommand = (command) => {
+				let entry = [];
+
+				if (command.options && command.options.length > 0) {
+					for (const option of command.options) {
+						if (option.type == ApplicationCommandOptionType.Subcommand) {
+							entry.push(`  - </${command.name} ${option.name}:${command.id}>: ${option.description}`);
+						} else if (option.type == ApplicationCommandOptionType.SubcommandGroup) {
+							entry.push(`  - \`/${command.name} ${option.name}\``);
+
+							for (const subOption of option.options) {
+								if (subOption.type == ApplicationCommandOptionType.Subcommand) entry.push(`    - </${command.name} ${option.name} ${subOption.name}:${command.id}>: ${subOption.description}`);
+							}
+						}
+					}
+				}
+
+				if (entry.length > 0) entry.unshift(`- \`/${command.name}\``);
+				else entry.unshift(`- </${command.name}:${command.id}>: ${command.description}`);
+
+				return entry.join("\n");
+			};
+
 			const globalSlashCommandList = [];
 			for (const command of globalCommands.values()) {
 				let valid = command.type == ApplicationCommandType.ChatInput;
 				if (command.contexts && interaction.context) valid &&= command.contexts.includes(interaction.context);
 				if (interaction.inGuild() && command.defaultMemberPermissions) valid &&= interaction.memberPermissions.has(command.defaultMemberPermissions);
-				if (valid) globalSlashCommandList.push(`- </${command.name}:${command.id}>: ${command.description}`);
+
+				if (valid) globalSlashCommandList.push(displayCommand(command));
 			}
 
 			const guildSlashCommandList = [];
@@ -40,18 +70,21 @@ const info = {
 				let valid = command.type == ApplicationCommandType.ChatInput;
 				if (command.contexts && interaction.context) valid &&= command.contexts.includes(interaction.context);
 				if (interaction.inGuild() && command.defaultMemberPermissions) valid &&= interaction.memberPermissions.has(command.defaultMemberPermissions);
-				if (valid) guildSlashCommandList.push(`- </${command.name}:${command.id}>: ${command.description}`);
+
+				if (valid) guildSlashCommandList.push(displayCommand(command));
 			}
 
-			await interaction.followUp(helpMessage
-				.replace("{{GLOBALSLASHCOMMANDS}}", globalSlashCommandList.length > 0 ? globalSlashCommandList.join("\n") : "There's currently no global commands.")
-				.replace("{{GUILDSLASHCOMMANDS}}", guildSlashCommandList.length > 0 ? guildSlashCommandList.join("\n") : `There's currently no guild commands for ${guild.name}.`)
-				.replaceAll("{{BOTID}}", bot.user.id)
-				.replaceAll("{{GUILDNAME}}", guild.name)
-				.replaceAll("{{DEFAULTTEXTCHANNEL}}", bot.settings.application.defaultTextChannel)
-				.replaceAll("{{ROLESCHANNEL}}", bot.settings.application.commands.roles.channel)
-				.replaceAll("{{TICKETSCHANNEL}}", bot.settings.application.commands.tickets.channel)
-			);
+			await sendLongMessage((options) => interaction.followUp(options), {
+				content: helpMessage
+					.replace("{{GLOBALSLASHCOMMANDS}}", globalSlashCommandList.length > 0 ? globalSlashCommandList.join("\n") : "There's currently no global commands.")
+					.replace("{{GUILDSLASHCOMMANDS}}", guildSlashCommandList.length > 0 ? guildSlashCommandList.join("\n") : `There's currently no guild commands for ${guild.name}.`)
+					.replaceAll("{{BOTID}}", bot.user.id)
+					.replaceAll("{{GUILDNAME}}", guild.name)
+					.replaceAll("{{DEFAULTTEXTCHANNEL}}", bot.settings.application.defaultTextChannel)
+					.replaceAll("{{ROLESCHANNEL}}", bot.settings.application.commands.roles.channel)
+					.replaceAll("{{TICKETSCHANNEL}}", bot.settings.application.commands.tickets.channel),
+				flags: MessageFlags.Ephemeral
+			});
 		} else logger.error("Help file missing");
 	}
 };
