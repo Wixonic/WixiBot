eb00e8d52f5584dd0eb2c15f4ba86e590b7abcd8
Wixonic <contact@wixonic.fr>
2025-08-19 18:49:16 +0200

    Update OBS logic

--- src/website/handlers/obs/settings.js
+++ src/website/handlers/obs/settings.js
@@ -8,89 +8,158 @@ const deviceList = {
 	video: []
 };
 
+/** @param {string} name */
 const trimName = (name) => name.split("(")[0].replace(/\s\n\t/, " ").trim();
 
-const updateDeviceList = () => {
-	const result = childProcess.spawnSync("/usr/local/ffmpeg-4.1/bin/ffmpeg", [
-		"-f", "avfoundation",
-		"-list_devices", "true",
-		"-i", ""
-	], { encoding: "utf8" });
+/** @param {import("@wixonic/logger").Logger} logger */
+const updateDeviceList = (logger) => {
+	try {
+		const result = childProcess.spawnSync("/usr/local/ffmpeg-4.1/bin/ffmpeg", [
+			"-f", "avfoundation",
+			"-list_devices", "true",
+			"-i", ""
+		], { encoding: "utf8", stderr: "pipe" });
 
-	deviceList.audio = [];
-	deviceList.video = [];
-	const output = (result.stderr || result.stdout).split("AVFoundation audio devices");
+		deviceList.audio = [];
+		deviceList.video = [];
+		const output = (result.stderr || result.stdout).split("AVFoundation audio devices");
 
-	for (const match of output[0].matchAll(/\.*\] \[(\d+)\] (.+)/g)) deviceList.video[Number(match[1])] = trimName(match[2]);
-	for (const match of output[1].matchAll(/\.*\] \[(\d+)\] (.+)/g)) deviceList.audio[Number(match[1])] = trimName(match[2]);
+		for (const match of output[0].matchAll(/\.*\] \[(\d+)\] (.+)/g)) deviceList.video[Number(match[1])] = trimName(match[2]);
+		for (const match of output[1].matchAll(/\.*\] \[(\d+)\] (.+)/g)) deviceList.audio[Number(match[1])] = trimName(match[2]);
+	} catch (e) {
+		logger.error("Failed to list devices:", e);
+	}
 };
 
-const defaultArgs = [
+const defaultInputArgs = [
 	"-hide_banner",
 	"-loglevel", "warning",
-
 	"-fflags", "nobuffer+genpts",
-	"-flags", "low_delay",
-	"-max_muxing_queue_size", "1024"
+	"-flags", "low_delay"
 ];
 
+const defaultOutputArgs = [
+	"-hide_banner",
+	"-loglevel", "warning",
+	"-fflags", "nobuffer",
+	"-probesize", "32",
+	"-analyzeduration", "0",
+	"-autoexit"
+];
+
+/** @param {number} port */
+const udpOutput = (port) => ([
+	"-flush_packets", "1",
+	"-muxdelay", "0",
+	"-muxpreload", "0",
+	"-f", "mpegts",
+	`udp://10.0.0.1:${port}`
+]);
+
+/** @typedef {{spawn: () => childProcess.ChildProcess, process: childProcess.ChildProcess?, active: boolean, name: string}} CaptureProcess */
+
 /**
- * @type {{[name: string]: {spawn: (logger: import("@wixonic/logger").Logger) => childProcess.ChildProcess, process: childProcess.ChildProcess?, active: boolean, name: string}}}
+ * @type {{[name: string]: CaptureProcess}}
  */
 const captureProcess = {
 	microphone: {
 		active: false,
 		name: "Microphone capture",
-		spawn: (logger) => {
-			logger.info("Starting process:", captureProcess.microphone.name);
-			return childProcess.spawn("/usr/local/ffmpeg-4.1/bin/ffmpeg", [
-				...defaultArgs,
+		spawn: () => {
+			const deviceIndex = deviceList.audio.indexOf("Elgato Wave:3");
+			if (deviceIndex == -1) return null;
+			else return childProcess.spawn("/usr/local/ffmpeg-4.1/bin/ffmpeg", [
+				...defaultInputArgs,
 
 				"-f", "avfoundation",
 				"-framerate", "60",
-				"-i", `:${deviceList.audio.indexOf("Elgato Wave:3")}`,
+				"-i", `:${deviceIndex}`,
 
 				"-vn",
 
-				"-c:a", "aac",
-				"-b:a", "320k",
-				"-ac", "1",
-				"-ar", "48000",
+				"-c:a", "aac", "-b:a", "320k", "-ac", "1", "-ar", "48000",
+
 				"-af", "volume=0.5",
 
-				"-f", "mpegts",
-				`udp://10.0.0.2:2003`
-			], { stdio: "inherit" });
+				...udpOutput(2003)
+			]);
 		},
 		process: null
 	},
 	audio: {
 		active: false,
 		name: "Audio",
-		spawn: (logger) => {
-			logger.info("Starting process:", captureProcess.audio.name);
-			return childProcess.spawn("ffplay", [
-				...defaultArgs,
+		spawn: () => childProcess.spawn("ffplay", [
+			...defaultOutputArgs,
 
-				"-autoexit",
-				"-nodisp",
-				"-vn",
+			"-autoexit",
+			"-nodisp",
+			"-vn",
 
-				"-f", "mpegts",
-				"udp://10.0.0.2:2000"
-			], { stdio: "inherit" });
-		},
+			"-f", "mpegts",
+			"udp://10.0.0.1:2003"
+		]),
 		process: null
 	},
 	broadcast: {
 		active: false,
 		name: "Broadcast",
-		spawn: (logger, settings) => {
-			logger.info("Starting process:", captureProcess.broadcast.name);
-			return null;
-		},
+		spawn: () => null,
 		process: null
 	}
+};
+
+/**
+ * @param {import("@wixonic/logger").Logger} logger
+ * @param {CaptureProcess} cp
+ * @param {import("../../types.d.ts").Settings} settings
+ */
+const startProcess = (logger, cp, settings) => {
+	const cpLogger = {
+		debug: (...args) => logger.debug(`[${cp.name}]`, ...args),
+		error: (...args) => logger.error(`[${cp.name}]`, ...args),
+		info: (...args) => logger.info(`[${cp.name}]`, ...args),
+		warn: (...args) => logger.warn(`[${cp.name}]`, ...args)
+	};
+
+	cp.process = cp.spawn();
+
+	if (cp.process instanceof childProcess.ChildProcess) {
+		cp.process.on("error", (error) => cpLogger.warn(error));
+		cp.process.on("exit", (code, signal) => {
+			if (signal == "SIGTERM") cpLogger.info("Stopped");
+			else cpLogger.warn(`Exited with code ${code} and signal ${signal}.`);
+			cp.process = null;
+		});
+		cp.process.stderr?.on("data", (data) => cpLogger.warn(data.toString().trim()));
+		cp.process.stdout?.on("data", (data) => cpLogger.debug(data.toString().trim()));
+	}
+};
+
+/**
+ * @param {import("@wixonic/logger").Logger} logger
+ * @param {CaptureProcess} cp
+ */
+const stopProcess = (logger, cp) => {
+	const cpLogger = {
+		debug: (...args) => logger.debug(`[${cp.name}]`, ...args),
+		error: (...args) => logger.error(`[${cp.name}]`, ...args),
+		info: (...args) => logger.info(`[${cp.name}]`, ...args),
+		warn: (...args) => logger.warn(`[${cp.name}]`, ...args)
+	};
+
+	if (!cp.process || cp.process.killed) return;
+	cpLogger.info("Stopping");
+
+	cp.process.kill("SIGTERM");
+
+	const killTimeout = setTimeout(() => {
+		if (!cp.process.killed) {
+			cpLogger.warn(`Did not respond to SIGTERM.Forcing kill with SIGKILL.`);
+			cp.process.kill("SIGKILL");
+		}
+	}, 3000);
+	cp.process.once("exit", () => clearTimeout(killTimeout));
 };
 
 /**
@@ -149,19 +218,12 @@ const info = {
 	},
 	loop: {
 		delay: 2 * 1000,
-		process: async (logger, settings, bot, rpc, sdk) => {
-			updateDeviceList();
+		process: (logger, settings) => {
+			updateDeviceList(logger);
 
 			for (const cp of Object.values(captureProcess)) {
-				if (!cp.process || cp.process.killed) {
-					if (cp.active) {
-						cp.process = cp.spawn(logger);
-						cp.process.on("error", (error) => logger.warn("Child Process Error:", error));
-					}
-				} else if (!cp.process.killed && !cp.active) {
-					logger.info("Killing process:", cp.name);
-					cp.process.kill("SIGTERM");
-				}
+				if (cp.active && !cp.process) startProcess(logger, cp, settings);
+				else if (!cp.active && cp.process) stopProcess(logger, cp);
 			}
 
 			return false;
