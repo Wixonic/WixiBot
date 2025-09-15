5ae9b521ee6df22b691b71986554ad6f33da690f
Wixonic <contact@wixonic.fr>
2025-09-15 19:02:42 +0200

    Update KCMaths

--- src/website/kcmaths/index.js
+++ src/website/kcmaths/index.js
@@ -9,67 +9,74 @@ addEventListener("DOMContentLoaded", async () => {
 
 	const params = new URLSearchParams(location.search);
 	let category = params.get("category") ?? "percent";
+	let date = params.get("date") ? new Date(params.get("date")) : new Date();
+	let mode = "before";
+	const history = [];
 
-	const load = async () => {
+	const load = async (checkNearest = true) => {
 		main.innerHTML = "";
+
 		const nav = document.createElement("nav");
 		nav.classList.add("fade");
 
 		{
-			const bank = document.createElement("button");
-			bank.classList.add("button");
-			bank.disabled = category == "bank";
-			bank.innerHTML = "Banque";
-			bank.addEventListener("click", async () => {
-				if (!bank.disabled) {
-					bank.disabled = true;
-					category = "bank";
-					load();
-				}
-			});
-			nav.append(bank);
-
-			const entries = document.createElement("button");
-			entries.classList.add("button");
-			entries.disabled = category == "entries";
-			entries.innerHTML = "Participations";
-			entries.addEventListener("click", async () => {
-				if (!entries.disabled) {
-					entries.disabled = true;
-					category = "entries";
-					load();
-				}
-			});
-			nav.append(entries);
-
-			const percent = document.createElement("button");
-			percent.classList.add("button");
-			percent.disabled = category == "percent";
-			percent.innerHTML = "Pourcentage";
-			percent.addEventListener("click", async () => {
-				if (!percent.disabled) {
-					percent.disabled = true;
-					category = "percent";
-					load();
-				}
-			});
-			nav.append(percent);
-
-			const victories = document.createElement("button");
-			victories.classList.add("button");
-			victories.disabled = category == "victories";
-			victories.innerHTML = "Victoires";
-			victories.addEventListener("click", async () => {
-				if (!victories.disabled) {
-					victories.disabled = true;
-					category = "victories";
-					load();
-				}
-			});
-			nav.append(victories);
+			// Nav
 		}
 
 		main.append(nav);
+
+		const sameDay = (a, b) => a.getUTCDate() == b.getUTCDate() && a.getUTCMonth() == b.getUTCMonth() && a.getUTCFullYear() == b.getUTCFullYear();
+
+		const displayDateControls = (entryDate, failed) => {
+			const dateControls = document.createElement("nav");
+			dateControls.classList.add("fade", "slide");
+
+			const before = document.createElement("button");
+			before.classList.add("button");
+			before.disabled = failed;
+			before.innerHTML = "Précédent";
+			before.addEventListener("click", async () => {
+				if (!before.disabled) {
+					before.disabled = true;
+					mode = "before";
+					date.setUTCDate(date.getUTCDate() - 1);
+					load();
+				}
+			});
+			dateControls.append(before);
+
+			const min = new Date(entryDate.getTime());
+			min.setUTCDate(min.getUTCDate() - 28);
+			const max = new Date();
+
+			const entryDateField = document.createElement("input");
+			entryDateField.type = "date";
+			entryDateField.id = "entryDate";
+			entryDateField.min = `${min.getUTCFullYear()}-${String(min.getUTCMonth() + 1).padStart(2, "0")}-${String(min.getUTCDate()).padStart(2, "0")}`;
+			entryDateField.value = `${entryDate.getUTCFullYear()}-${String(entryDate.getUTCMonth() + 1).padStart(2, "0")}-${String(entryDate.getUTCDate()).padStart(2, "0")}`;
+			entryDateField.max = `${max.getUTCFullYear()}-${String(max.getUTCMonth() + 1).padStart(2, "0")}-${String(max.getUTCDate()).padStart(2, "0")}`;
+			entryDateField.addEventListener("input", () => {
+				date = entryDateField.valueAsDate;
+				load(false);
+			});
+			dateControls.append(entryDateField);
+
+			const after = document.createElement("button");
+			after.classList.add("button");
+			after.disabled = sameDay(new Date(), date);
+			after.innerHTML = "Suivant";
+			after.addEventListener("click", async () => {
+				if (!after.disabled) {
+					after.disabled = true;
+					mode = "after";
+					date.setUTCDate(date.getUTCDate() + 1);
+					load();
+				}
+			});
+			dateControls.append(after);
+
+			return dateControls;
+		};
 
 		const createMemberEntry = (id, data) => {
 			const member = document.createElement("a");
@@ -111,23 +118,9 @@ addEventListener("DOMContentLoaded", async () => {
 
 		const getIdFromDate = (date) => String(date.getDate()).padStart(2, "0") + String(date.getMonth() + 1).padStart(2, "0");
 
-		let date = new Date();
-		let leaderboardRequest = await request("GET", new URL(`/kcmaths/api/?date=${getIdFromDate(date)}`, localEnvironment ? path.local.server : path.server), "json", "application/json", null, 600);
-
-		while (leaderboardRequest.status == 404) {
-			date.setUTCDate(date.getUTCDate() - 1);
-			leaderboardRequest = await request("GET", new URL(`/kcmaths/api/?date=${getIdFromDate(date)}`, localEnvironment ? path.local.server : path.server), "json", "application/json", null, 600);
-			if (Date.now() - date.getTime() > 28 * 24 * 60 * 60 * 1000) break;
-		}
-
-		if (leaderboardRequest.status == 200) {
-			const leaderboard = leaderboardRequest.response;
-			for (const id in leaderboard) {
-				leaderboard[id].percent = leaderboard[id].entries > 0 ? leaderboard[id].victories / leaderboard[id].entries : 0;
-				leaderboard[id].id = id;
-			}
-
-			const sortedLeaderboard = Object.values(leaderboard).sort((a, b) => {
+		const displayLeaderboard = async (entry) => {
+			const entryDate = new Date(entry.date);
+			const sortedLeaderboard = Object.values(entry.leaderboard).sort((a, b) => {
 				switch (category) {
 					case "bank":
 						if (a.kcCoins == b.kcCoins) return b.percent - a.percent;
@@ -150,7 +143,7 @@ addEventListener("DOMContentLoaded", async () => {
 				};
 			});
 
-			await (async () => {
+			{
 				const section = document.createElement("section");
 				section.classList.add("fade");
 				main.append(section);
@@ -160,10 +153,66 @@ addEventListener("DOMContentLoaded", async () => {
 				title.innerHTML = "KCMaths";
 				section.append(title);
 
-				const lastUpdated = document.createElement("p");
-				lastUpdated.classList.add("fade", "slide");
-				lastUpdated.innerHTML = `Mis à jour le: ${String(date.getDate()).padStart(2, "0")}/${String(date.getMonth() + 1).padStart(2, "0")}/${date.getFullYear()}`;
-				section.append(lastUpdated);
+				const sortingFilters = document.createElement("nav");
+				sortingFilters.classList.add("fade", "slide");
+
+				{
+					const bank = document.createElement("button");
+					bank.classList.add("button");
+					bank.disabled = category == "bank";
+					bank.innerHTML = "Banque";
+					bank.addEventListener("click", async () => {
+						if (!bank.disabled) {
+							bank.disabled = true;
+							category = "bank";
+							load();
+						}
+					});
+					sortingFilters.append(bank);
+
+					const entries = document.createElement("button");
+					entries.classList.add("button");
+					entries.disabled = category == "entries";
+					entries.innerHTML = "Participations";
+					entries.addEventListener("click", async () => {
+						if (!entries.disabled) {
+							entries.disabled = true;
+							category = "entries";
+							load();
+						}
+					});
+					sortingFilters.append(entries);
+
+					const percent = document.createElement("button");
+					percent.classList.add("button");
+					percent.disabled = category == "percent";
+					percent.innerHTML = "Pourcentage";
+					percent.addEventListener("click", async () => {
+						if (!percent.disabled) {
+							percent.disabled = true;
+							category = "percent";
+							load();
+						}
+					});
+					sortingFilters.append(percent);
+
+					const victories = document.createElement("button");
+					victories.classList.add("button");
+					victories.disabled = category == "victories";
+					victories.innerHTML = "Victoires";
+					victories.addEventListener("click", async () => {
+						if (!victories.disabled) {
+							victories.disabled = true;
+							category = "victories";
+							load();
+						}
+					});
+					sortingFilters.append(victories);
+				}
+
+				section.append(sortingFilters);
+
+				section.append(displayDateControls(entryDate));
 
 				const leaderboardContainer = document.createElement("div");
 				leaderboardContainer.classList.add("leaderboard", "fade", "slide");
@@ -175,8 +224,59 @@ addEventListener("DOMContentLoaded", async () => {
 				}
 
 				section.append(leaderboardContainer);
-			})();
-		} else main.innerHTML += "Failed to fetch recent data";
+			}
+		};
+
+		const saved = history.find((entry) => sameDay(new Date(entry.date), date));
+
+		if (saved) displayLeaderboard(saved);
+		else {
+			const initialDate = date.getTime();
+
+			let leaderboardRequest = await request("GET", new URL(`/kcmaths/api/?date=${getIdFromDate(date)}`, localEnvironment ? path.local.server : path.server), "json", "application/json", null, 600);
+
+			if (checkNearest) {
+				while (leaderboardRequest.status == 404) {
+					date.setUTCDate(date.getUTCDate() + (mode == "after" ? 1 : -1));
+
+					if (date.getTime() > Date.now()) date = new Date();
+					if (initialDate - date.getTime() > 28 * 24 * 60 * 60 * 1000) break;
+
+					leaderboardRequest = await request("GET", new URL(`/kcmaths/api/?date=${getIdFromDate(date)}`, localEnvironment ? path.local.server : path.server), "json", "application/json", null, 600);
+				}
+			}
+
+			if (leaderboardRequest.status == 200) {
+				const leaderboard = leaderboardRequest.response;
+				for (const id in leaderboard) {
+					leaderboard[id].percent = leaderboard[id].entries > 0 ? leaderboard[id].victories / leaderboard[id].entries : 0;
+					leaderboard[id].id = id;
+				}
+
+				const entry = {
+					date: date.getTime(),
+					leaderboard
+				};
+
+				history.push(entry);
+				displayLeaderboard(entry);
+			} else {
+				const section = document.createElement("section");
+				section.classList.add("fade");
+				main.append(section);
+
+				const title = document.createElement("h2");
+				title.classList.add("fade", "slide");
+				title.innerHTML = "KCMaths";
+				section.append(title);
+
+				section.append(displayDateControls(date, true));
+
+				const message = document.createElement("div");
+				message.innerHTML = "Failed to fetch data for this period.";
+				section.append(message);
+			};
+		};
 	};
 
 	await load();
