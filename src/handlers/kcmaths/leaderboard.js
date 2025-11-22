1b749d2fe44450947ca94fe499a0961d2efe90ca
Wixonic <contact@wixonic.fr>
2025-11-22 20:53:36 +0100

    Update

--- src/handlers/kcmaths/leaderboard.js
+++ src/handlers/kcmaths/leaderboard.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const fsp = require("fs/promises");
 const path = require("path");
 
+const modifiers = require("./modifiers.json");
+
 /**
  * @type {import("../../types.d.ts").HandlerInfo}
  */
@@ -10,6 +12,7 @@ const info = {
 	handlers: {
 		get: async (logger, settings, req, res, bot, rpc, sdk) => {
 			const date = new Date(req.query.date);
+			date.setUTCHours(0, 0, 0);
 
 			if (Number.isNaN(date.getDate())) {
 				res.status(400).json({
@@ -20,8 +23,17 @@ const info = {
 				const exists = fs.existsSync(filePath);
 
 				if (exists) {
-					const leaderboard = await fsp.readFile(filePath, "utf-8");
-					res.status(200).json(JSON.parse(leaderboard));
+					const leaderboard = JSON.parse(await fsp.readFile(filePath, "utf-8"));
+					for (const id in modifiers) {
+						for (const field in modifiers[id]) {
+							for (const modifier of modifiers[id][field]) {
+								const modifierDate = new Date(modifier[0]);
+								modifierDate.setUTCHours(0, 0, 0);
+								if (date.getTime() >= modifierDate.getTime()) leaderboard[id][field] += modifier[1];
+							}
+						}
+					}
+					res.status(200).json(leaderboard);
 				} else res.status(404).json([]);
 			}
 		}
